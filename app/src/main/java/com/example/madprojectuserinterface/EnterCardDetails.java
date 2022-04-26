c7b94b3aa19c29e9e5dd950530b9b08af4146fd5
rajitha-mahesh <it20653804@my.sliit.lk>
2022-04-26 17:50:30 +0530

    first 2

---
+++ app/src/main/java/com/example/madprojectuserinterface/EnterCardDetails.java
@@ -0,0 +1,14 @@
+package com.example.madprojectuserinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterCardDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_card_details);
+    }
+}
---
+++ app/src/main/java/com/example/madprojectuserinterface/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.madprojectuserinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.cardpay);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openactivity_enter_cd();
+            }
+        });
+
+    }
+    public void openactivity_enter_cd(){
+        Intent intent = new Intent(this, EnterCardDetails.class);
+        startActivity(intent);
+    }
+}
