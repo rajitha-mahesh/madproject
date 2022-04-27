2d8b2704aea621859b4257ce80e50e8ebeeb512b
rajitha-mahesh <it20653804@my.sliit.lk>
2022-04-27 10:35:42 +0530

    payment

---
+++ app/src/main/java/com/example/madprojectuserinterface/enter_transfer_detaill.java
@@ -0,0 +1,14 @@
+package com.example.madprojectuserinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enter_transfer_detaill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_transfer_detaill);
+    }
+}
