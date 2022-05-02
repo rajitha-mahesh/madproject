ee66d361bcac00382e3560106433ca42a7525ac6
IT20651688 <IT20651688@mt.sliit.lk>
2022-05-02 17:07:44 +0530

    commit update

--- app/src/main/java/com/example/madprojectuserinterface/MainActivity.java
+++ app/src/main/java/com/example/madprojectuserinterface/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.madprojectuserinterface;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,12 +15,27 @@ public class MainActivity extends AppCompatActivity {
 
     private Button changeToCardPay;
     private Button changeToBankTransfer;
+    private DrawerLayout drawer;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //nav bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         changeToCardPay =  findViewById(R.id.cardbtn);
         changeToBankTransfer =  findViewById(R.id.banktransferbtn);
@@ -39,5 +58,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+        @Override
+        public void onBackPressed() {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            }else {
+                super.onBackPressed();
+            }
 
     }
+}
