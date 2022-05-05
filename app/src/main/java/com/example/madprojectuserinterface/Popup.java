e672f52f5c9830c4be6e4bb9f17ebb445e73c175
resh0223 <it20646974@my.sliit.lk>
2022-05-05 16:25:07 +0530

    commit update

--- app/src/main/java/com/example/madprojectuserinterface/Popup.java
+++ app/src/main/java/com/example/madprojectuserinterface/Popup.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ConfirmPassword extends AppCompatActivity {
+public class Popup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_password);
+        setContentView(R.layout.activity_popup);
     }
 }
