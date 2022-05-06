c28d5ff475d55240634e1464f99c1066d6e062a2
IT20651688 <IT20651688@mt.sliit.lk>
2022-05-06 11:34:47 +0530

    commit update

--- app/src/main/java/com/example/madprojectuserinterface/TestModule1.java
+++ app/src/main/java/com/example/madprojectuserinterface/TestModule1.java
@@ -2,13 +2,98 @@ package com.example.madprojectuserinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TestModule1 extends AppCompatActivity {
+
+    private Dialog dialog;
+    private Button ShowDialog1;
+    private Button ShowDialog2;
+    private Button ShowDialog3;
+    private Button ShowDialog4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_module1);
+
+        ShowDialog1 = findViewById(R.id.com_mths_btn);
+        ShowDialog2 = findViewById(R.id.phy_btn);
+        ShowDialog3 = findViewById(R.id.chem_btn);
+        ShowDialog4 = findViewById(R.id.it_btn);
+
+        //Create the Dialog here
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.activity_popup);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_dialog_background));
+        }
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false); //Optional
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; //Setting the animations to dialog
+
+        Button Okay = dialog.findViewById(R.id.btn_okay);
+        Button Cancel = dialog.findViewById(R.id.btn_cancel);
+
+        Okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(TestModule1.this, "Okay", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(TestModule1.this, "Cancel", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+
+        ShowDialog1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.show(); // Showing the dialog here
+            }
+        });
+
+        ShowDialog2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.show(); // Showing the dialog here
+            }
+        });
+
+        ShowDialog3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.show(); // Showing the dialog here
+            }
+        });
+
+        ShowDialog4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.show(); // Showing the dialog here
+            }
+        });
+
+
     }
+
 }
