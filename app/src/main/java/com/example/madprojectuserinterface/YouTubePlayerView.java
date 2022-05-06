5b2f5af5d52d295276a12bb081f5cedc5e5a2a62
IT20651688 <IT20651688@mt.sliit.lk>
2022-05-07 02:38:28 +0530

    commit update

---
+++ app/src/main/java/com/example/madprojectuserinterface/YouTubePlayerView.java
@@ -0,0 +1,14 @@
+package com.example.madprojectuserinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YouTubePlayerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_you_tube_player_view);
+    }
+}
