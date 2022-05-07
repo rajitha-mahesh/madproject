c457c7a2bb6bd06a7448a08961aa378f60cff721
resh0223 <it20646974@my.sliit.lk>
2022-05-07 12:18:35 +0530

    commit update

---
+++ app/src/main/java/com/example/madprojectuserinterface/ComMathPastPapers.java
@@ -0,0 +1,14 @@
+package com.example.madprojectuserinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComMathPastPapers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com_math_past_papers);
+    }
+}
--- app/src/main/java/com/example/madprojectuserinterface/TestModule1.java
+++ app/src/main/java/com/example/madprojectuserinterface/TestModule1.java
@@ -1,7 +1,9 @@
 package com.example.madprojectuserinterface;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class TestModule1 extends AppCompatActivity {
@@ -10,5 +12,21 @@ public class TestModule1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_module1);
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem)
+
+                switch (item.getItemId()) {
+                    case R.id.nav_in_module;
+                    startActivity(new Intent(getApplicationContext(), InModule.class));
+                    break;
+
+
+                }
+
+                return true;
     }
+
+
 }
