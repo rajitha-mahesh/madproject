11388df4185ab063290e257800655749e035c980
IT20651688 <IT20651688@mt.sliit.lk>
2022-05-07 22:43:19 +0530

    commit update

--- app/src/main/java/com/example/madprojectuserinterface/MainHome.java
+++ app/src/main/java/com/example/madprojectuserinterface/MainHome.java
@@ -2,13 +2,101 @@ package com.example.madprojectuserinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainHome extends AppCompatActivity {
+
+
+    //maths
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+
+    //resource
+    private  Button button5;
+    private  Button button6;
+    private  Button button7;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
+
+        button1 = findViewById(R.id.mathe_btn);
+        button2 = findViewById(R.id.biolgy_btn);
+        button3 = findViewById(R.id.chem_btn);
+        button4 = findViewById(R.id.it_btn);
+
+        //resource
+        button5 = findViewById(R.id.pp_btn);
+        button6 = findViewById(R.id.ebook_btn);
+        button7 = findViewById(R.id.video_btn);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainHome.this, TestModule1.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainHome.this, TestModule1.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainHome.this, TestModule1.class);
+                startActivity(intent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainHome.this, TestModule1.class);
+                startActivity(intent);
+            }
+        });
+
+        //resource
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainHome.this, TestModule1.class);
+                startActivity(intent);
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainHome.this, TestModule1.class);
+                startActivity(intent);
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainHome.this, TestModule1.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
